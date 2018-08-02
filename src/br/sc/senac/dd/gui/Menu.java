bed65febf33fceb74aafd739731f70f32da9feac
Guilherme Corrêa <Guilherme Corra@172.28.3.148>
2018-08-01 21:10:13 -0300

    Primeira Classe

---
+++ src/br/sc/senac/dd/gui/Menu.java
@@ -0,0 +1,13 @@
+package br.sc.senac.dd.gui;
+
+import javax.swing.JOptionPane;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		
+		JOptionPane.showConfirmDialog(null, "Hello World!");
+
+	}
+
+}
---
+++ src/br/sc/senac/dd/gui/Test.java
@@ -0,0 +1,5 @@
+package br.sc.senac.dd.gui;
+
+public class Test {
+
+}
