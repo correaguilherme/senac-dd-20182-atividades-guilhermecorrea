a416af779097f8a96189f117d2acdd8792f63d26
Guilherme Corrêa <Guilherme Corra@LENOVO>
2018-10-13 20:30:23 -0300

    Aula 10

---
+++ src/br/sc/senac/dd/aula10/view/CadastroCliente.java
@@ -0,0 +1,14 @@
+package br.sc.senac.dd.aula10.view;
+
+import javax.swing.JPanel;
+
+public class CadastroCliente extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public CadastroCliente() {
+
+	}
+
+}
